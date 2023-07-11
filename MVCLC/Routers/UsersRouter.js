ed58ff0be5630eba09603047a97b3a2f1d7acffc
Yalagala Srinivas <srinivas.y@lionorbit.com>
2023-07-12 02:15:24 +0530

    another

--- MVCLC/Routers/UsersRouter.js
+++ MVCLC/Routers/UsersRouter.js
@@ -75,7 +75,6 @@ userRouter.post('/login',(req,res,next)=>{
     res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
      Users.findOne({email:req.body.email})
-     .exec()
      .then(user=>{
         console.log(user);
         if(user.length==0){
