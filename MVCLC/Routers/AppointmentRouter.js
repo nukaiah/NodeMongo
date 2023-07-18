a13d01a8838785cef2bfe7f3a682888a1d7b301d
Yalagala Srinivas <srinivas.y@lionorbit.com>
2023-07-18 15:22:49 +0530

    Error Log

--- MVCLC/Routers/AppointmentRouter.js
+++ MVCLC/Routers/AppointmentRouter.js
@@ -105,7 +105,7 @@ appointmentRouter.post('/addAppointment', async(req,res,next)=>{
     // });
    } catch (e) {
     res.status(500).json({
-        error:"ERROR"+e,
+        error:e,
         status:false,
         message:"Failed Add An Appointment"
     })
