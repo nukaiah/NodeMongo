07e1af0126bb4db56b3537e2671cc7ad10f4b815
Yalagala Srinivas <srinivas.y@lionorbit.com>
2023-07-24 15:26:44 +0530

    Patient View Unit Test is Done

--- MVCLC/Routers/FileUploaderRouter.js
+++ MVCLC/Routers/FileUploaderRouter.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const fileRouter = express.Router();
-// const multer = require('multer');
-// const upload = multer({ dest: 'tmp/' }); // 
+
 const cloudinary = require('cloudinary').v2;
+
+const multer = require('multer');
+const upload = multer({ dest: 'tmp/',limits: {
+  fileSize: 10 * 1024 * 1024, // 10MB limit
+}, }); 
 
 
 cloudinary.config({
@@ -11,18 +15,21 @@ cloudinary.config({
     api_secret: 'OAekp042IQNVaY63p0122vZAsRk'
 });
 
-fileRouter.post('/uploadFile', async(req, res, next) => {
+fileRouter.post('/uploadFile',  async(req, res, next) => {
+ 
+
+
     // Check if files were uploaded
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).json({ error: 'No files were uploaded.'});
     }
     else{
     const uploadedFile = req.files.file;
-    // const result = await cloudinary.uploader.upload().end(uploadedFile.data);
-    // console.log(result);
+    // // const result = await cloudinary.uploader.upload().end(uploadedFile.data);
+    // // console.log(result);
     res.status(200).json({
         status:true,
-        path:uploadedFile.tempFilePath,
+        path:uploadedFile,
         message:"File Uploaded Successfully"
     });
   
--- app.js
+++ app.js
@@ -33,9 +33,7 @@ mongoose.connection.on("connected",connected=>{
 });
 
 app.use(cors());
-app.use(fileUpload({
-  useTempFiles:true
-}));
+app.use(fileUpload());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
