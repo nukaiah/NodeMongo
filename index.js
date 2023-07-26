49d970cfb1357efc9dd95901edb78332f1bd7edb
Yalagala Srinivas <srinivas.y@lionorbit.com>
2023-07-27 01:29:34 +0530

    Issues Logs

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 require('dotenv').config();
 var app = require('./app');
-var createServer = http.createServer(app);
+const server = http.createServer(app,(req, res) => {
+    // Set CORS headers
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // ... Handle your API request logic here ...
+  
+    // Send the response
+    res.end("Hello, world!");
+  });
 
 createServer.listen(port,console.log("Server Created Successfully"));
