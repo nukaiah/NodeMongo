149eac953aa1b78050bb64838ff110b00665fcd2
Yalagala Srinivas <srinivas.y@lionorbit.com>
2024-01-23 13:02:20 +0530

    Yamadonga

--- MVCLC/Routers/XLSXRouter.js
+++ MVCLC/Routers/XLSXRouter.js
@@ -7,44 +7,30 @@ const MlaVisits = require('../Models/MLAVisitModels');
 
 xlsxRouter.use(fileUpload());
 
-xlsxRouter.post('/MlaXlxsUpload', checkAuth, async (req, res, next) => {
+xlsxRouter.post('/MlaXlxsUpload', checkAuth,async (req, res, next) => {
   try {
+    // Assuming the file is sent as 'file' in the request
     const file = req.files.file;
     const workbook = xlsx.read(file.data, { type: 'buffer' });
+
     const sheetName = workbook.SheetNames[0];
     const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
-    const dataWithIds = sheetData.map(record => ({ createdBy: req.userId, _id: new mongoose.Types.ObjectId(), ...record }));
+    const dataWithIds = sheetData.map(record => ({ createdBy: req.userId, ...record }));
 
-    // Check for duplicates and insert only unique records
-    for (const record of dataWithIds) {
-      const query = {
-        dateSA: record.dateSA,
-        mandal: record.mandal,
-        village: record.village,
-        purposeVisit: record.purposeVisit,
-      };
+    console.log(dataWithIds);
 
-      const existingRecord = await MlaVisits.findOne(query);
-
-      if (!existingRecord) {
-        // Record doesn't exist, insert it
-        await MlaVisits.create(record);
-      } else {
-        // Log or handle the case where the record already exists
-        console.log('Record already exists:', existingRecord);
-      }
-    }
-
+    await MlaVisits.insertMany(dataWithIds);
+    
     return res.status(200).json({
       status: true,
-      message: 'Data uploaded successfully',
+      message: 'Data uploaded successfully'
     });
   } catch (err) {
     console.error(err);
     return res.status(500).json({
       status: false,
-      message: 'Failed to upload data',
+      message: 'Failed to upload data'
     });
   }
 });
