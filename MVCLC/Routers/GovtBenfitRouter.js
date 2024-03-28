ca8a0ad22956eb368b8129c00f9175143845294f
Yalagala Srinivas <srinivas.y@lionorbit.com>
2024-03-28 16:55:18 +0530

    Govt Issue

--- MVCLC/Routers/GovtBenfitRouter.js
+++ MVCLC/Routers/GovtBenfitRouter.js
@@ -67,6 +67,7 @@ govtBenfitRouter.get('/getAll',checkAuth,async (req,res,next)=>{
         res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         var result = await govtSchema.find();
+        console.log(result);
         if(result){
             res.status(200).json({
                 status:true,
