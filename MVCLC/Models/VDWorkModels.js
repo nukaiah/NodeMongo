3f8b85831fd0dd587f54ac613d59570eb454536b
Yalagala Srinivas <srinivas.y@lionorbit.com>
2024-03-30 13:19:19 +0530

    Village Leaders

--- MVCLC/Models/VDWorkModels.js
+++ MVCLC/Models/VDWorkModels.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var VDWorkSchema = new mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
-    date:Date,
+    date:String,
     mandal:String,
     village:String,
     stateOrCentralSchemaL:String,
@@ -12,8 +12,8 @@ var VDWorkSchema = new mongoose.Schema({
     inchargeofWork:String,
     inchargePhone:String,
     workStatus:String,
-    workStartDate:Date,
-    workEndDate:Date,
+    workStartDate:String,
+    workEndDate:String,
     amountStatus:String,
     amountSpent:String,
     amountSanction:String,
